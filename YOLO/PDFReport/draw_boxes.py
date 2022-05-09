9e4f4ef64f7ce770e75152630e04f85169f92654
Michael de Lagarde <michael.delagarde@delair.local>
2022-05-09 18:51:23 +0200

    Debugged PDF annotation report

--- YOLO/PDFReport/draw_boxes.py
+++ YOLO/PDFReport/draw_boxes.py
@@ -3,25 +3,30 @@ from matplotlib import cm
 import matplotlib.pyplot as plt
 
 
-classes = ['croquette0', 'croquette1']
+def draw_boxes(img, boxes, labels, class_names, scores):
 
-def draw_boxes(img, boxes, labels, scores):
+	size = img.shape
+	print(size)
 
-	for label, [x1, y1, x2, y2], score in zip(labels, boxes, scores):
+	for label, [x1, y1, x2, y2], class_name, score in zip(labels, boxes, class_names, scores):
+
+		ycv1 = size[0]-y1
+		ycv2 = size[0]-y2
+		xcv1 = x1
+		xcv2 = x2
+
 		cmap_boxes = cm.get_cmap("Set1") # colour map
 		rgba = cmap_boxes(int(label%9))
 		bgr = rgba[2]*255, rgba[1]*255, rgba[0]*255
-		# cv2.rectangle(frame, (int(x1), int(y1)), (int(x2), int(y2)), color=bgr, thickness=1)
-		# cv2.putText(frame, classes[int(label)] + '  ' + str(round(score,4)) , (int(x1), int(y1)+8), cv2.FONT_HERSHEY_SIMPLEX, 0.3, bgr, 1)
 
 		#box
-		line_thickness=3
+		line_thickness=5
 		tl = line_thickness or round(0.002 * (img.shape[0] + img.shape[1]) / 2) + 1  # line/font thickness
-		c1, c2 = (int(x1), int(y1)), (int(x2), int(y2))
+		c1, c2 = (int(xcv1), int(ycv1)), (int(xcv2), int(ycv2))
 		cv2.rectangle(img, c1, c2, color=bgr, thickness=tl, lineType=cv2.LINE_AA)
 
 		#label on boxes
-		txt = classes[int(label)] + '  ' + str(round(score,2))
+		txt = class_name # + '  ' + str(round(score,2))
 		tf = max(tl - 1, 1)  # font thickness
 		t_size = cv2.getTextSize(txt, 0, fontScale=tl / 3, thickness=tf)[0]
 		c2 = c1[0] + t_size[0], c1[1] - t_size[1] - 3
--- YOLO/PDFReport/pdf_report.py
+++ YOLO/PDFReport/pdf_report.py
@@ -3,15 +3,22 @@ from matplotlib.backends.backend_pdf import PdfPages
 import os
 import cv2
 from math import floor
-from draw_boxes import draw_boxes
+import sys
+sys.path.insert(0, '..')
+from PDFReport.draw_boxes import draw_boxes
+from AlteiaTransactions.ExportAnnotations import convert_annotations
 import pandas as pd
+import alteia
+from pathlib import Path
 
 
-def pdf_report(df_annotations, images_dir)
+
+
+def pdf_report(df_annotations, images_dir, WORKING_DIR):
 
 
 	# The PDF document
-	pdf = PdfPages('foo.pdf')
+	pdf = PdfPages(WORKING_DIR / 'Annotations_report.pdf')
 
 	nrows = 3
 	ncols = 2
@@ -19,21 +26,16 @@ def pdf_report(df_annotations, images_dir)
 	fig, ax = plt.subplots(figsize=(8.27, 11.69), dpi=100, nrows=nrows, ncols=ncols)
 
 	images = os.listdir(images_dir)
-	# images = [x for x in images if '.JPG' in x]
-	print(images)
 
-	bbox = [150,150, 300, 300]
-	color = 'blue'
-	boxes = [bbox]
-	labels = [1]
-	scores = [0.8]
 
-	p = 0
+	p = 0 #current page of the pdf file
 
-	for k in range(len(images)):
+	# for k in range(len(images)):
+	for k in range(6):
 
 		if floor(k/(nrows*ncols))>p:
 			pdf.savefig(fig)
+			plt.close('all')
 			fig, ax = plt.subplots(figsize=(8.27, 11.69), dpi=100, nrows=nrows, ncols=ncols)
 			p+=1
 
@@ -42,24 +44,52 @@ def pdf_report(df_annotations, images_dir)
 		a = floor(kp/ncols)
 		b = kp%ncols
 
-		img = cv2.imread(images[k])
+		img = cv2.imread(str(images_dir / images[k]))
 		img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 
-		select = df_annotations[df_annotations['image_name']==images[k]]
-		boxes = [[r['x1'], r['y1'], r['x1'], r['y1']] for r in select.iterrows()]
-		class_names = select['score'].tolist()
+		select = df_annotations[df_annotations['image_name']==images[k][:-4]]
+		boxes = [[r['x1'], r['y1'], r['x2'], r['y2']] for _,r in select.iterrows()]
+		labels = select['class'].tolist()
+		class_names = select['class_name'].tolist()
 		scores = select['score'].tolist()
 
-		img = draw_boxes(img, boxes, labels, scores)
+		img = draw_boxes(img, boxes, labels, class_names, scores)
+
+		# plt.imshow(img)
+		# plt.show()
 
 		ax[a][b].imshow(img)
-		ax[a][b].set_title(images[k])
+		ax[a][b].set_title(images[k], fontdict={'fontsize': 7})
 
+	pdf.savefig(fig)
+
+	#make a histogram of total number of each annotation
+	fig, ax = plt.subplots(figsize=(8.27, 11.69), dpi=100, nrows=2, ncols=1)
+
+
+	# pivot = df_annotations.pivot(index='class_names', values='class_names', aggfunc='count')
+	class_names = df_annotations['class_name'].drop_duplicates().tolist()
+	counts = [df_annotations[df_annotations['class_name']==c].shape[0] for c in class_names]
+
+	ax[0].bar(range(len(counts)), counts, tick_label=class_names)
+	ax[0].set_xticklabels(class_names, rotation=45, ha='right', fontdict={'fontsize': 7})
 	pdf.savefig(fig)
 
 	pdf.close()
 
 
+
+
 if __name__ == "__main__":
 
-	pdf_report(df_annotations, images_dir)
+	sdk = alteia.SDK(config_path='./config-connections.json')
+	# project = sdk.projects.search(filter={'name': {'$eq': 'Demo_datascience'}})[0]
+	# mission = sdk.missions.search(filter={'project': {'$eq': project.id}})[0]
+
+	WORKING_DIR = Path('./').resolve()
+	annotations_dir = Path('C:\\Users\\michael.delagarde\\Documents\\DEV\\DemoDataScience\\chess\\valid')
+
+	df_annotations = convert_annotations('6242fedbc55a3c0007254175', '6277e95a1aef750007602a56', annotations_format='yolo', annotations_dir=annotations_dir)
+	# export_annotations(df_annotations)
+	print(df_annotations.columns)
+	pdf_report(df_annotations, images_dir=annotations_dir/'images', WORKING_DIR=Path('./'))
